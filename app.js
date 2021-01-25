cdf11c4bce05020bd8447b27c59ab972bd6641f9
Aya berjawi <ayabarjawi@gmail.com>
2021-01-25 22:43:19 +0200

    anime

--- app.js
+++ app.js
@@ -7,15 +7,88 @@ var listArray=[];
 function Anime(animeName,animeCategory,animieseason){
     this.animeName=animeName;
     this.animeCategory=animeCategory;
-    this.animieseason=getRandomInt(1,8);
+    this.animieseason=getRandomInt();
+
     listArray.push(this);
 }
 
+/*Anime.prototype.renderItems =function(){
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
+    var animeRow = document.createElement('tr');
+
+    var itemName = document.createElement('td');
+    itemName.textContent=this.animeName;
+    animeRow.appendChild(itemName);
+
+    var itemCategory = document.createElement('td');
+    itemCategory.textContent=this.animeCategory;
+    animeRow.appendChild(itemCategory);
+
+    var itemSeason = document.createElement('td');
+    itemSeason.textContent=this.animieseason;
+    animeRow.appendChild(itemSeason);
+
+  
+
+}*/
+
+function renderList(){
+    renderHeader();
+
+    for (let index = 0; index < listArray.length; index++) {
+        
+    
+    var animeRow = document.createElement('tr');
+
+    var itemName = document.createElement('td');
+    itemName.textContent=listArray[index].animeName;
+    animeRow.appendChild(itemName);
+
+    var itemCategory = document.createElement('td');
+    itemCategory.textContent=listArray[index].animeCategory;
+    animeRow.appendChild(itemCategory);
+
+    var itemSeason = document.createElement('td');
+    itemSeason.textContent=listArray[index].animieseason;
+    animeRow.appendChild(itemSeason);
+
+    /// deletbtn
+
+    var animeTableRowRemoveBtn = document.createElement('button');
+    animeTableRowRemoveBtn.textContent = 'X';
+    animeTableRowRemoveBtn.setAttribute('id', index);
+
+    animeTableRowRemoveBtn.addEventListener('click', deleteAnimeFromList);
+    animeRow.appendChild(animeTableRowRemoveBtn)
+
+    parrentTable.appendChild(animeRow);
+
+
+
+    }
+
+
+
+}
+
+function renderHeader(){
+    var headerRow = document.createElement('tr');
+    var th;
+
+    for (let index = 0; index < headercontent.length; index++) {
+        th = document.createElement('th');
+        th.textContent=headercontent[index];
+        headerRow.appendChild(th)
+        
+    }
+    parrentTable.appendChild(headerRow);
+}
+
+
+function getRandomInt() {
+    min = Math.ceil(1);
+    max = Math.floor(8);
+    return Math.floor(Math.random() * (8 - 1) + 1); 
   }
 
 
@@ -24,12 +97,41 @@ function submitData(event){
 
     var animeName = event.target.animeTitleInput.value;
     var animeCategory = event.target.animeCategory.value;
-    var animieseason= getRandom(1,8);
+
+    var newList = new Anime(animeName,animeCategory)
+    //newList.renderItems();
+
+
+    localStorage.setItem('animeList',JSON.stringify(listArray));
+
+    // deletbtn
+    var animeTable = document.getElementById('animeListTable');
+    animeTable.innerHTML = '';
+    renderList();
+    var addAnimeForm = document.getElementById('addAnimeToWL');
+    addAnimeForm.reset();
+
 
 
 
 }
+function checkList(){
+    if (localStorage.getItem('animeList')){
+        listArray=JSON.parse(localStorage.getItem('animeList'))
+        renderList();
+    }
+}
+
+/// deletbtn
+function deleteAnimeFromList(event){
+    var selectedAnime = event.target.id;
+    listArray.splice(selectedAnime, 1);
+    localStorage.setItem('animeList', JSON.stringify(listArray));
+    var animeTable = document.getElementById('animeListTable');
+    animeTable.innerHTML = '';
+    renderList();
+  }
 
 
-
-ourForm.addEventListener('submit',submitData)
+ourForm.addEventListener('submit',submitData);
+checkList();
