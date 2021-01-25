ab6415bf29fc57688e2e7c8b808bc2dc06cdd661
Aya berjawi <ayabarjawi@gmail.com>
2021-01-25 20:27:49 +0200

    anime

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+var headercontent = ['Anime Title','Category','Random Season','Remove'];
+var parrentTable = document.getElementById('animeListTable');
+var ourForm = document.getElementById('addAnimeToWL')
+var listArray=[];
 
+
+function Anime(animeName,animeCategory,animieseason){
+    this.animeName=animeName;
+    this.animeCategory=animeCategory;
+    this.animieseason=getRandomInt(1,8);
+    listArray.push(this);
+}
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+  }
+
+
+function submitData(event){
+    event.preventDefault();
+
+    var animeName = event.target.animeTitleInput.value;
+    var animeCategory = event.target.animeCategory.value;
+    var animieseason= getRandom(1,8);
+
+
+
+}
+
+
+
+ourForm.addEventListener('submit',submitData)
